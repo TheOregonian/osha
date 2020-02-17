23265db45596c8c28ad6b0de6680390cd7d0c430
ssuo1 <ssuo@oregonian.com>
2020-02-17 12:45:20 -0800

    Adding shell script for running on cron job.

---
+++ processing.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /usr/local/webapps/osha
+/usr/local/webapps/osha/ENV/bin/python "/usr/local/webapps/osha/OSHA processing.py"
+#/usr/local/webapps/osha/ENV/bin/python "/usr/local/webapps/osha/OSHA processing.py" >/usr/local/webapps/osha/osha.log 2>&1
